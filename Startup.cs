a9b0e94828ea926893dfa2452a2b861b30691df1
Pedro Sanchez <reisereise.85@gmail.com>
2017-10-16 19:07:01 +0100

    Add MVC Framework services
    Also add it to the request execution pipeline

--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,7 @@ namespace CityInfo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,13 +43,7 @@ namespace CityInfo
                 app.UseExceptionHandler();
             }
 
-            app.Run((context) => {
-                throw new Exception("huuuu");
-            });
-
-            //app.Run(async (context) => {
-            //    await context.Response.WriteAsync("Hello World");
-            //});
+            app.UseMvc();
         }
     }
 }
